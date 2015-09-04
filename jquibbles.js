7f2d78a20ecde959b67a1cea7ed349415a0be5e4
nickrenfo2 <nickrenfo@gmail.com>
2015-09-03 21:08:28 -0500

    Updated for rereresume challenge
    Resume is now initially hidden, and will fade in after clicking the
    button.

--- jquibbles.js
+++ jquibbles.js
@@ -5,9 +5,17 @@ function readyFn( jQuery ) {
     //alert("Hello World");
     $('.slideoutBox').click( expand );
     //$('#workXP').click( expand );
+    $('main').hide();
+    $('.showbutton').click(showDocument);
 }
 
 function expand () {   
     console.log("Expand");
     $(this).toggleClass("expanded");
+}
+
+function showDocument() {
+    $('.showbutton').fadeOut(1000);
+    $('main').delay(1000).fadeIn('slow');
+    
 }
